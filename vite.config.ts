1594fda5c22ff11cd137c2d33a1d38eef7543fa7
Hammadsoomro <165611609+Hammadsoomro@users.noreply.github.com>
2025-09-09 17:58:42 +0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,36 @@
-import { defineConfig, Plugin } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { type Plugin } from "vite";
+import { type ViteDevServer } from "vite";
+import { type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { createServer } from "./server";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+export default defineConfig(
+  ({ mode }): UserConfig => ({
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-  build: {
-    outDir: "dist/spa",
-  },
-  plugins: [react(), expressPlugin()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
+    build: {
+      outDir: "dist/spa",
     },
-  },
-}));
+    plugins: [react(), expressPlugin()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./client"),
+        "@shared": path.resolve(__dirname, "./shared"),
+      },
+    },
+  }),
+);
 
 function expressPlugin(): Plugin {
   return {
     name: "express-plugin",
     apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
+    configureServer(server: ViteDevServer): void {
       const app = createServer();
 
       // Add Express app as middleware to Vite dev server
